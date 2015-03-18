f8e3f27ae14edf19950e0502b0bafa7f79d6be9b
Piotr Jura <piotr.jura22@gmail.com>
2015-03-18 16:50:50 +0100

    Stworzenie nowego projektu MVC 5
    Czysty projekt w MVC 5

---
+++ Portal Kulinarny/Portal Kulinarny/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Portal_Kulinarny
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
