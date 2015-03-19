4fe12d2d9cc2802c780b2077367f8be259d03598
Piotr Jura <piotr.jura22@gmail.com>
2015-03-19 10:49:04 +0100

    Dodanie modelu

---
+++ Portal Kulinarny/Portal Kulinarny/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal_Kulinarny.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public int RecipeId { get; set; }
+        public int AuthorId { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Portal Kulinarny/Portal Kulinarny/Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal_Kulinarny.Models
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public int AuthorId { get; set; }
+        public string Content { get; set; }
+        public double Rating { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
