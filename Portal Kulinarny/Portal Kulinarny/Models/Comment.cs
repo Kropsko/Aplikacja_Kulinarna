8f2f2ed0d64d45ded23eb108f65b734de3b99eec
Piotr Jura <piotr.jura22@gmail.com>
2015-03-25 21:34:27 +0100

    Zmiana modelu i schematu kolorów Bootstrapa

--- Portal Kulinarny/Portal Kulinarny/Models/Comment.cs
+++ Portal Kulinarny/Portal Kulinarny/Models/Comment.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Portal_Kulinarny.Models
+﻿namespace Portal_Kulinarny.Models
 {
     public class Comment
     {
         public int CommentId { get; set; }
         public int RecipeId { get; set; }
-        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
         public string Content { get; set; }
     }
 }
