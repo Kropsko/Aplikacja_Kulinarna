edfb83c1f80b551e2b01e1182fd9a7f7d8cbeeaa
Piotr Jura <piotr.jura22@gmail.com>
2015-06-10 13:00:27 +0200

    Poprawione formy jednostek i czasow

--- Portal Kulinarny/Portal Kulinarny/Controllers/RecipesController.cs
+++ Portal Kulinarny/Portal Kulinarny/Controllers/RecipesController.cs
@@ -12,6 +12,7 @@ using System.IO;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Portal_Kulinarny.Models.Constants;
+using System.Globalization;
 
 namespace Portal_Kulinarny.Controllers
 {
@@ -208,6 +209,185 @@ namespace Portal_Kulinarny.Controllers
             var userRecipes = db.Recipes.Where(a => a.AuthorName == User.Identity.Name).ToList();
 
             return View(userRecipes);
+        }
+
+        public string ValidWordForm(string unitName, string quantity)
+        {
+            if (quantity.Contains(','))
+            {
+                quantity = quantity.Replace(",", ".");
+            }
+            else if (quantity.Contains('/'))
+            {
+                string[] numbers = quantity.Split('/');
+                quantity = (double.Parse(numbers[0]) / double.Parse(numbers[1])).ToString(CultureInfo.InvariantCulture);
+            }
+            else if (quantity.Contains('-'))
+            {
+                string[] numbers = quantity.Split('-');
+                quantity = double.Parse(numbers[1]).ToString(CultureInfo.InvariantCulture);
+            }
+            double quantityNumber = double.Parse(quantity, CultureInfo.InvariantCulture);
+
+            string validForm = unitName;
+            switch (unitName)
+            {
+                case "litr":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "litry";
+                    else if (quantityNumber >= 5)
+                        validForm = "litrów";
+                    else if (quantityNumber > 0 && quantityNumber < 1)
+                        validForm = "litra";
+                    break;
+
+                case "mililitr":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "mililitry";
+                    else if (quantityNumber >= 5)
+                        validForm = "mililitrów";
+                    break;
+
+                case "kilogram":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "kilogramy";
+                    else if (quantityNumber >= 5)
+                        validForm = "kilogramów";
+                    break;
+
+                case "dekagram":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "dekagramy";
+                    else if (quantityNumber >= 5)
+                        validForm = "dekagramów";
+                    break;
+
+                case "gram":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "gramy";
+                    else if (quantityNumber >= 5)
+                        validForm = "gramów";
+                    break;
+
+                case "sztuka":
+                    if (Math.Abs(quantityNumber - 1) > 0.00000001 && quantityNumber > 0 && quantityNumber < 5)
+                        validForm = "sztuki";
+                    else if (quantityNumber >= 5)
+                        validForm = "sztuk";
+                    break;
+
+                case "plaster":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "plastry";
+                    else if (quantityNumber >= 5)
+                        validForm = "plastrów";
+                    else if (quantityNumber > 0 && quantityNumber < 1)
+                        validForm = "plastra";
+                    break;
+
+                case "opakowanie":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "opakowania";
+                    else if (quantityNumber >= 5)
+                        validForm = "opakowań";
+                    break;
+
+                case "łyżka":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "łyżki";
+                    else if (quantityNumber >= 5)
+                        validForm = "łyżek";
+                    break;
+
+                case "łyżeczka":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "łyżeczki";
+                    else if (quantityNumber >= 5)
+                        validForm = "łyżeczek";
+                    else if (quantityNumber > 0 && quantityNumber < 1)
+                        validForm = "łyżeczeki";
+                    break;
+
+                case "szklanka":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "szklanki";
+                    else if (quantityNumber >= 5)
+                        validForm = "szklanek";
+                    else if (quantityNumber > 0 && quantityNumber < 1)
+                        validForm = "szklanki";
+                    break;
+
+                case "kropla":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "krople";
+                    else if (quantityNumber >= 5)
+                        validForm = "kropli";
+                    else if (quantityNumber > 0 && quantityNumber < 1)
+                        validForm = "kropli";
+                    break;
+
+                case "kostka":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "kostki";
+                    else if (quantityNumber >= 5)
+                        validForm = "kostek";
+                    else if (quantityNumber > 0 && quantityNumber < 1)
+                        validForm = "kostki";
+                    break;
+
+
+                case "szczypta":
+                    if (quantityNumber > 1 && quantityNumber < 5)
+                        validForm = "szczypty";
+                    else if (quantityNumber >= 5)
+                        validForm = "szczypt";
+                    break;
+
+            }
+            return validForm;
+        }
+
+        public string ValidMinutesForm(int minutes)
+        {
+            if (minutes < 100)
+            {
+                if (minutes == 1)
+                    return "minuta";
+
+                if (minutes <= 4)
+                    return "minuty";
+
+                if (minutes > 4 && minutes <= 21)
+                    return "minut";
+
+                if (minutes > 21)
+                {
+                    if (minutes % 10 > 4 || minutes % 10 == 0 || minutes % 10 == 1)
+                        return "minut";
+                    else
+                        return "minuty";
+                }
+            }
+            else
+            {
+                if (minutes == 100 || minutes == 101)
+                    return "minut";
+
+                if (minutes <= 104)
+                    return "minuty";
+
+                if (minutes > 104 && minutes < 122)
+                    return "minut";
+
+                if (minutes > 121)
+                {
+                    if (minutes % 100 > 4 || minutes % 100 == 0 || minutes % 100 == 1)
+                        return "minut";
+                    else
+                        return "minuty";
+                }
+            }
+            return "minuta";
         }
 
     }
